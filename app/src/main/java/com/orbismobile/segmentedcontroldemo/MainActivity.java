d4b2d0d31cb3f95a49f43f3e61eac3cb32c48328
Carlos Leonardo Camilo <carlosleonardovargashuaman@gmail.com>
2017-08-24 12:37:56 -0500

    clear unused code

--- app/src/main/java/com/orbismobile/segmentedcontroldemo/MainActivity.java
+++ app/src/main/java/com/orbismobile/segmentedcontroldemo/MainActivity.java
@@ -1,7 +1,6 @@
 package com.orbismobile.segmentedcontroldemo;
 
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,19 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.orbismobile.segmentedcontrol.SegmentEntity;
 import com.orbismobile.segmentedcontrol.SegmentedControl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SegmentedControl.OnSegmentClickListener {
-    List<Object> segmentEntities;
-    SegmentedControl scGeneral;
+    private List<Object> segmentEntities;
+    private SegmentedControl scGeneral;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
